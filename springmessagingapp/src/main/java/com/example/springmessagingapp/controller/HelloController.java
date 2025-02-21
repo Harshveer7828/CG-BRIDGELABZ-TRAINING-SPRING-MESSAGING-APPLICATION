069ccb5471b27cd40e780d1dd57f4385f8ce384c
harshveer7828 <harshveer7828@gmail.com>
2025-02-21 21:45:22 +0530

    [Harsh veer singh] Add. Add a new feature of collecting data from the url using path variable✨

--- springmessagingapp/src/main/java/com/example/springmessagingapp/controller/HelloController.java
+++ springmessagingapp/src/main/java/com/example/springmessagingapp/controller/HelloController.java
@@ -1,4 +1,5 @@
 package com.example.springmessagingapp.controller;
+
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -7,10 +8,20 @@ public class HelloController {
 
     // GET request to respond hello message to the user
     @GetMapping
-    public String hello(){
+    public String hello() {
         return "Hello From BridgeLabz";
     }
 
+    // GET request to extract the name from the query parameter
+    @GetMapping("/query")
+    public String sayHello(@RequestParam String name){
+        return "Hello " + name + " from BridgeLabz";
+    }
 
+    // GET request to extract the name from path variables
+    @GetMapping("{name}")
+    public String sayAgainHello(@PathVariable String name){
+        return "Hello " + name + " from BridgeLabz";
+    }
 
 }
